f54fa783aba7d444ecef7c34e79e9961067c4019
cralva <caa081297@gmail.com>
2025-08-02 09:55:17 -0400

    Started args

---
+++ 36Args&Kwargs.py
@@ -0,0 +1,5 @@
+# *args           = allows you to pass multiple non-key arguments
+# **kwargs        = allows you to pass multiple keyword-arguments
+#                  * unpacking operator
+#                 1. positional 2. default 3. keyword 4. ARBITRARY
+
