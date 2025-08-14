d6bbb722344f8d1aa56650ab9484ce8de0bc5897
cralva <caa081297@gmail.com>
2025-08-13 22:24:27 -0400

    Started the list comprehension

---
+++ 39listComprehensions.py
@@ -0,0 +1,4 @@
+# List comprehension = A concise way to create lists in python
+#                      Compact and easier to read than traditional loops
+#                     [expression for value in iterable if condition]
+
