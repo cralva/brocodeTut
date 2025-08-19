1e2081b1ba8f21eed798f150106c6acb4b9b79eb
cralva <caa081297@gmail.com>
2025-08-19 08:20:16 -0400

    SaVED FILE

---
+++ 42name==main.py
@@ -0,0 +1,4 @@
+# if __name == __main__: (this script can be imported OR run standalone)
+#                        functions and classes in this module can be reused
+#                        without the main block of code executing
+
